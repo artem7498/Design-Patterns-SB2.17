dd498a707364a630623d959644f656a29471e99f
Artem Akopian <artem7498@gmail.com>
2021-04-22 20:08:59 +1000

    Initial Commit

---
+++ Design Patterns SB2.17/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Design Patterns SB2.17
+//
+//  Created by Артём on 4/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
